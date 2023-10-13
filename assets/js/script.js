d654b136ccbed7abb2bac79ccb26382965d4f53f
Felipe Castillo <felipecastillo48@gmail.com>
2023-10-13 19:41:51 -0300

    Fourth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ const searchBtn = document.querySelector(".search-btn");
 const convertedAmount = document.querySelector(".result");
 const choiceParent = document.querySelector("#currency-select");
 let displayChart;
-async function getdata(currency) {
+async function getdata() {
   try {
-    const res = await fetch(`https://mindicador.cl/api/${currency}`);
+    const res = await fetch(`https://mindicador.cl/api/${choiceParent.value}`);
     if (!res.ok) {
       throw new Error(`Error: ${res.status}`);
     }
@@ -19,30 +19,33 @@ async function getdata(currency) {
   }
 }
 
-function getSelectedCurrency() {
-  let value = getdata(choiceParent.value);
-  return value;
-}
-
-/* function formatCurrency(number) {
-  const uf = new Intl.NumberFormat("es-CL", {
+function formatCurrency(number) {
+  const currencies = [
+    { name: "uf", code: "es-CL", currency: "CLF" },
+    { name: "dolar", code: "en-US", currency: "USD" },
+    { name: "euro", code: "de-DE", currency: "EUR" },
+  ];
+  const currencyUnit = currencies.filter(
+    (datum) => datum.name === choiceParent.value
+  );
+  const [{ code, currency }] = currencyUnit;
+  const formattedCurrency = new Intl.NumberFormat(`${code}`, {
     style: "currency",
-    currency: "CLF",
+    currency: `${currency}`,
   }).format(number);
-  return uf;
-} */
+  return formattedCurrency;
+}
 
 async function carryOutCalculation() {
   try {
-    const value = await getSelectedCurrency();
-    const latestValue = value.serie.map((val) => {
+    const value = await getdata();
+    const latestTenValues = value.serie.map((val) => {
       return val.valor;
     });
     const enteredValue = +input.value;
-    const calc = enteredValue / latestValue[0];
-    convertedAmount.innerHTML = `Resultado: ${choiceParent.value} ${calc
-      .toFixed(2)
-      .toLocaleString("es-CL")}`;
+    const [latestValue] = latestTenValues;
+    const calc = enteredValue / latestValue;
+    convertedAmount.innerHTML = `Resultado: ${formatCurrency(calc)}`;
   } catch (error) {
     console.log(error.message);
   }
@@ -58,7 +61,7 @@ function getChartData(value) {
       labels: labels,
       datasets: [
         {
-          label: "Historial últimos 10 días",
+          label: `${choiceParent.value}: Historial últimos 10 días `,
           data: data,
           backgroundColor: "black",
           borderColor: "black",
@@ -70,7 +73,7 @@ function getChartData(value) {
   return config;
 }
 
-function updateChart(chart) {
+function clearChart(chart) {
   if (chart) {
     chart.destroy();
   }
@@ -78,11 +81,10 @@ function updateChart(chart) {
 
 async function renderChart() {
   try {
-    const value = await getSelectedCurrency();
+    const value = await getdata();
     const chart = document.getElementById("chart").getContext("2d");
     const config = getChartData(value);
-    /*     if (displayChart) displayChart.destroy(); */
-    updateChart(displayChart);
+    clearChart(displayChart);
     displayChart = new Chart(chart, config);
   } catch (error) {
     console.log(error.message);
@@ -96,6 +98,7 @@ searchBtn.addEventListener("click", () => {
   ) {
     carryOutCalculation();
     renderChart();
+    formatCurrency();
     chart.style.backgroundColor = "white";
   } else if (input.value === "" || isNaN(input.value)) {
     alert("Ingresa un valor valido");
